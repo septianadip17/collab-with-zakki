c7db52bb8379fdafdd3e22c4fa267dcbe318f01b
Septian Adi Pratama <septianadip7998@gmail.com>
2025-02-25 17:47:48 +0700

    navbar showed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import logo from '../assets/ecodoe-logo.png'
+import React from "react";
+import logo from "../assets/ecodoe-logo.png";
 
 const Navbar = () => {
   return (
-    <div className='container'>
-      <div>
-        <ul>
-          <li><img src={logo} alt="" /></li>
-          <li>BERANDA</li>
-          <li>TENTANG KAMI</li>
-          <li>UMKM</li>
-          <li>PORTOFOLIO</li>
-          <li>PRODUK</li>
-          <li>BLOG</li>
-          <li>KARIR</li>
-          <li>KONTAK</li>
+    <nav className="container mx-auto p-4">
+      <div className="flex items-center justify-between">
+        <ul className="flex space-x-6 mx-auto">
+          <li>
+            <img src={logo} alt="Ecodoe Logo" className="h-10" />
+          </li>
+          <li className="hover:text-gray-700 cursor-pointer">BERANDA</li>
+          <li className="hover:text-gray-700 cursor-pointer">TENTANG KAMI</li>
+          <li className="hover:text-gray-700 cursor-pointer">UMKM</li>
+          <li className="hover:text-gray-700 cursor-pointer">PORTOFOLIO</li>
+          <li className="hover:text-gray-700 cursor-pointer">PRODUK</li>
+          <li className="hover:text-gray-700 cursor-pointer">BLOG</li>
+          <li className="hover:text-gray-700 cursor-pointer">KARIR</li>
+          <li className="hover:text-gray-700 cursor-pointer">KONTAK</li>
         </ul>
       </div>
-    </div>
-  )
-}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
