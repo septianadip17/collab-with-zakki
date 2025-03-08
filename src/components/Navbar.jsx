080497c9f911cb919c355501d42e14aa8e7d984a
Septian Adi Pratama <septianadip7998@gmail.com>
2025-03-09 03:09:06 +0700

    navbar humberger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/ecodoe-logo.png";
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="container mx-auto p-4">
-      <div className="flex items-center justify-between">
-        <ul className="flex space-x-6 mx-auto">
-          <li>
-            <img src={logo} alt="Ecodoe Logo" className="h-10" />
+    <nav className="border-b-2 border-gray-300 p-4">
+      <div className="sticky container mx-auto flex items-center justify-between">
+        {/* Menu Items */}
+        <ul className={`
+          md:flex md:space-x-6 md:items-center md:justify-between md:w-full
+          absolute top-16 left-0 w-full bg-white p-4 shadow-md md:static md:bg-transparent md:p-0
+          ${isOpen ? "block" : "hidden"}
+        `}>
+          <li className="md:mr-6">
+            <img src={logo} alt="Ecodoe Logo" className="h-12" />
           </li>
-          <li className="hover:text-gray-700 cursor-pointer">BERANDA</li>
-          <li className="hover:text-gray-700 cursor-pointer">TENTANG KAMI</li>
-          <li className="hover:text-gray-700 cursor-pointer">UMKM</li>
-          <li className="hover:text-gray-700 cursor-pointer">PORTOFOLIO</li>
-          <li className="hover:text-gray-700 cursor-pointer">PRODUK</li>
-          <li className="hover:text-gray-700 cursor-pointer">BLOG</li>
-          <li className="hover:text-gray-700 cursor-pointer">KARIR</li>
-          <li className="hover:text-gray-700 cursor-pointer">KONTAK</li>
+          {[
+            "BERANDA",
+            "TENTANG KAMI",
+            "UMKM",
+            "PORTOFOLIO",
+            "PRODUK",
+            "BLOG",
+            "KARIR",
+            "KONTAK",
+          ].map((item, index) => (
+            <li key={index} className="hover:text-gray-700 cursor-pointer py-2 md:py-0">
+              {item}
+            </li>
+          ))}
         </ul>
+
+        {/* Hamburger Menu */}
+        <button
+          className="md:hidden focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <FiX size={24} /> : <FiMenu size={24} />}
+        </button>
       </div>
     </nav>
   );
