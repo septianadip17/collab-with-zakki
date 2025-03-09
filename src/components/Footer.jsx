e07eb6b0ec845c21eaf92cd7f130c33625275308
Septian Adi Pratama <septianadip7998@gmail.com>
2025-03-10 01:48:11 +0700

    logo footer image added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { FaPhoneAlt, FaMapMarkerAlt, FaInstagram, FaTwitter, FaFacebook, FaYoutube, FaLinkedin } from "react-icons/fa";
+import footerLogo from "../assets/logo-footer.png"
+
 
 const Footer = () => {
   return (
-    <footer className="bg-green-700 text-white font-sans py-10">
+    <footer className="bg-[#147958] text-white font-sans py-10">
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-between">
-          <div className="w-full md:w-1/4 mb-6 md:mb-0">
+          <div className="w-full md:w-1/3 mb-6 md:mb-0">
             <img
-              src="https://storage.googleapis.com/a1aa/image/o7eWrWu2pHWh6bmzr_oNq376JUF508w--daRvVZ5oyI.jpg"
+              src={footerLogo}
               alt="Ecodoe logo"
-              className="mb-4"
-              width="100"
-              height="100"
+              className="mb-4 w-[214px] h-[64px]"
             />
-            <p className="mb-4">Jl Bukit Duri Tanjakan, Tanjakan DLM XV No. 50 RT.04/RW.08 Bukit Duri, Tebet, South Jakarta - Indonesia. Indonesia 12840</p>
+            <p className="mb-4 text-sm">Jl Bukit Duri Tanjakan, Tanjakan DLM XV No. 50 RT.04/RW.08 Bukit Duri, Tebet, South Jakarta - Indonesia. Indonesia 12840</p>
             <p className="mb-4 flex items-center"><FaPhoneAlt className="mr-2" /> (+6221) 21383994</p>
-            <button className="bg-white text-green-700 py-2 px-4 rounded-full flex items-center">
+            <button className="bg-white hover:bg-gray-100 text-green-700 py-2 px-4 rounded-full flex items-center">
               <FaMapMarkerAlt className="mr-2" /> Petunjuk Arah
             </button>
           </div>
           
-          <div className="w-full md:w-3/4 flex flex-wrap justify-between">
+          <div className="w-full md:w-2/3 flex flex-wrap justify-between">
             <div className="w-1/2 md:w-1/5 mb-6 md:mb-0">
               <h3 className="font-bold mb-4">TENTANG</h3>
               <ul>
@@ -72,8 +72,8 @@ const Footer = () => {
           <h3 className="font-bold mb-4">Berlangganan Ecodoe melalui Email</h3>
           <p className="mb-4">Dapatkan info dan promo menarik dari Ecodoe yang akan dikirimkan langsung ke email Anda</p>
           <div className="flex justify-center">
-            <input type="email" placeholder="tuliskan alamat email Anda" className="p-2 rounded-l-full text-gray-700" />
-            <button className="bg-white text-green-700 py-2 px-4 rounded-r-full">Langganan</button>
+            <input type="email" placeholder="tuliskan alamat email Anda" className="p-2 rounded-full text-gray-700" />
+            <button className="bg-white text-green-700 py-2 px-4 rounded-full">Langganan</button>
           </div>
         </div>
         
