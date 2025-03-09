faad9fbd2a958c6b11e460798778013a822c543b
Septian Adi Pratama <septianadip7998@gmail.com>
2025-03-09 13:26:24 +0700

    pages added

---
+++ src/pages/Contatct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contatct = () => {
+  return (
+    <div>Contatct</div>
+  )
+}
+
+export default Contatct
---
+++ src/pages/Umkm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Umkm = () => {
+  return (
+    <div>Umkm</div>
+  )
+}
+
+export default Umkm
