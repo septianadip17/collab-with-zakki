c797ed5030490e9c9a53d5976e73ac2dce22b04f
Septian Adi Pratama <septianadip7998@gmail.com>
2025-03-09 14:05:40 +0700

    logo image still small

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,42 +17,38 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="p-6 flex items-center justify-between shadow-md bg-white w-full z-50 relative top-0">
+    <nav className=" lg:p-6 flex items-center justify-between lg:justify-around shadow-md bg-white w-full z-50 relative top-0">
       {/* Logo */}
-      <div className="flex justify-center items-center">
-        <div className="items-center justify-center">
-
+      <div className="flex-1 flex lg:justify-end w-1/5">
         <img
           src={logo}
           alt="Ecodoe Logo"
-          className="h-16 object-contain cursor-pointer"
+          className="h-16 p-2 object-contain cursor-pointer"
         />
-        </div>
       </div>
 
       {/* Desktop Menu */}
-      <ul className="hidden md:flex items-center justify-center gap-x-6">
-        {menuItems.map((item, index) => (
-          <li
-            key={index}
-            className="hover:text-gray-700 hover:bg-gray-200 px-3 py-2 rounded cursor-pointer transition-colors text-lg"
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
+      <div className="flex w-4/5">
+        <ul className="hidden lg:flex items-center justify-around flex-1 gap-x-6">
+          {menuItems.map((item, index) => (
+            <li
+              key={index}
+              className="hover:text-gray-700 hover:bg-gray-200 px-3 py-2 rounded cursor-pointer transition-colors text-lg"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
 
       {/* Mobile Menu Button */}
-      <button
-        className="md:hidden focus:outline-none"
-        onClick={() => setIsOpen(!isOpen)}
-      >
+      <button className="lg:hidden w-auto p-4" onClick={() => setIsOpen(!isOpen)}>
         {isOpen ? <FiX size={24} /> : <FiMenu size={24} />}
       </button>
 
       {/* Mobile Menu */}
       <div
-        className={`absolute top-full left-0 w-full bg-white shadow-md p-4 transition-all duration-300 md:hidden ${
+        className={`absolute top-full left-0 w-full bg-white shadow-md p-4 transition-all duration-300 lg:hidden ${
           isOpen ? "block" : "hidden"
         }`}
       >
@@ -60,7 +56,7 @@ const Navbar = () => {
           {menuItems.map((item, index) => (
             <li
               key={index}
-              className="hover:text-gray-700 hover:bg-gray-200 px-3 py-2 rounded cursor-pointer transition-colors"
+              className="hover:text-gray-700 text-sm hover:bg-gray-200 px-3 py-2 rounded cursor-pointer transition-colors"
             >
               {item}
             </li>
